de24ac96fd1d541e2fec8ff92fae3011b35fbc8d
dogbutcat <dogbutcat@hotmail.com>
2018-11-28 22:50:37 -0800

    FIX: ss.json path error

--- script/final.sh
+++ script/final.sh
@@ -5,11 +5,11 @@ cat ss.json
 # add ip forward for centos-7
 echo 1 > /proc/sys/net/ipv4/ip_forward
 echo "net.ipv4.ip_forward = 1" > /etc/sysctl.conf
-/usr/bin/ssserver -c ./ss.json --workers ${WORKER_NUM} -d start
+/usr/bin/ssserver -c ../ss.json --workers ${WORKER_NUM} -d start
 /usr/sbin/sshd -D
 
 finish(){
-	/usr/bin/ssserver -c ./ss.json --workers ${WORKER_NUM} -d stop
+	/usr/bin/ssserver -c ../ss.json --workers ${WORKER_NUM} -d stop
 	exit 0
 }
 
